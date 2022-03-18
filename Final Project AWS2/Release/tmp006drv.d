500403edeacaaaf6a4924ca88896acb2a8c9ea58
Abraham <45084728+abrahamhadaf@users.noreply.github.com>
2022-03-18 14:15:40 -0700

    Add files via upload

---
+++ Final Project AWS2/Release/tmp006drv.d
@@ -0,0 +1,31 @@
+# FIXED
+
+tmp006drv.obj: ../tmp006drv.c
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/math.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_defs.h
+tmp006drv.obj: D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_limits.h
+tmp006drv.obj: ../tmp006drv.h
+tmp006drv.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/i2c_if.h
+tmp006drv.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/uart_if.h
+
+../tmp006drv.c: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/math.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_defs.h: 
+D:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_limits.h: 
+../tmp006drv.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/i2c_if.h: 
+C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/uart_if.h: 
